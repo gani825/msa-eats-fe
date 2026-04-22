812c74847dee700a10a6ecf58c8bd7c299a71783
gani825 <kgee0503@gmail.com>
2026-04-22 10:43:19 +0900

    refactor(api): orderService 함수명 통일, getOrderDetail 추가

--- src/api/orderService.js
+++ src/api/orderService.js
@@ -1,10 +1,22 @@
+// 주문 관련 API 서비스
 import axios from './httpRequester';
 
 const path = '/order';
 
-// 주문 생성 API 호출
-// data 예시: { items: [{ menuId, price, quantity }] }
-export const placeOrder = async data => {
+// 주문 생성 (totalAmount 포함 필수 - BE에서 금액 위변조 검증)
+export const postOrder = async data => {
     const res = await axios.post(path, data);
-    return res.data; // 생성된 Order ID 반환
+    return res.data;
+};
+
+// 주문 목록 조회 (커서 기반 페이지네이션: params = { last_id })
+export const getOrderList = async params => {
+    const res = await axios.get(path, { params });
+    return res.data;
+};
+
+// 주문 상세 조회 (orderId로 해당 주문의 메뉴명 포함 아이템 목록 조회)
+export const getOrderDetail = async orderId => {
+    const res = await axios.get(`${path}/${orderId}`);
+    return res.data;
 };
